ddec0fa0c53f3ca715780f958c4db891953123dc
Mathieu Mescam <mate@e.email>
2024-07-21 16:31:33 +0200

    Add controls at the head of the table to rank by play count

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,24 @@
 document.addEventListener('DOMContentLoaded', async () => {
+  let songs = await fetchSongs();
+  renderSongs(songs);
+
+  const sortableHeader = document.querySelector('.sortable');
+  sortableHeader.addEventListener('click', () => {
+    const sortedSongs = sortSongsByPlayCount(songs);
+    toggleSortIcon(sortableHeader);
+    renderSongs(sortedSongs);
+  });
+});
+
+async function fetchSongs() {
   const response = await fetch('/songs');
   const songs = await response.json();
+  return songs;
+}
+
+function renderSongs(songs) {
   const tableBody = document.querySelector('#songs-table');
+  tableBody.innerHTML = '';
 
   if (songs.length === 0) {
     const div = document.createElement('div');
@@ -21,4 +38,22 @@ document.addEventListener('DOMContentLoaded', async () => {
       tableBody.appendChild(div);
     });
   }
-});
+}
+
+function sortSongsByPlayCount(songs) {
+  return songs.sort((a, b) => b.play_count - a.play_count);
+}
+
+function toggleSortIcon(header) {
+  const icon = header.querySelector('i.fas');
+  if (icon.classList.contains('fa-sort')) {
+    icon.classList.remove('fa-sort');
+    icon.classList.add('fa-sort-down');
+  } else if (icon.classList.contains('fa-sort-down')) {
+    icon.classList.remove('fa-sort-down');
+    icon.classList.add('fa-sort-up');
+  } else if (icon.classList.contains('fa-sort-up')) {
+    icon.classList.remove('fa-sort-up');
+    icon.classList.add('fa-sort-down');
+  }
+}
